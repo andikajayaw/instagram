d3bfe23838f020efc4f95cc76baa31ef53bf4d28
it2 <it2@kayuraja.local>
2020-06-15 15:52:36 +0800

    update UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,33 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+        <div class="col-3 p-5">
+            <img src="https://instagram.fcgk25-1.fna.fbcdn.net/v/t51.2885-19/s150x150/103832811_744987189593339_3401429852540201249_n.jpg?_nc_ht=instagram.fcgk25-1.fna.fbcdn.net&_nc_ohc=2cvemFsvxa4AX8C_TyH&oh=33fa5b5ba06b1896e154a7b6d40f9462&oe=5F116B5C" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>andikajayaw</h1>
             </div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>19</strong> posts</div>
+                <div class="pr-4"><strong>771</strong> followers</div>
+                <div class="pr-4"><strong>670</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">Hello</div>
+            <div>Welcome to my Instagram cloning by Laravel 7.X</div>
+            <div><a href="#">andikajayaw.github.io</a></div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center pt-4">
+        <div class="col-4">
+            <img src="https://instagram.fcgk25-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/81227218_569504600423881_5002898419900133500_n.jpg?_nc_ht=instagram.fcgk25-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=71wwHXBqPbQAX88T6Xm&oh=ea7e1126207df4a0682313c719ed9447&oe=5F111643" alt="1" class="w-100 pt-4">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fcgk25-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/103491569_2731789523717671_2283239598610367560_n.jpg?_nc_ht=instagram.fcgk25-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=kpAOwpOXVEMAX-Irxyl&oh=c1de6f419a931e802a9e766355a11639&oe=5F128485" alt="1" class="w-100 pt-4">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fcgk25-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.135.1080.1080a/s640x640/103186636_264985311250138_8208966331772493100_n.jpg?_nc_ht=instagram.fcgk25-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=m57SpCey_CMAX_d5MKK&oh=9f4ff33db884c18a7e85ba48ca063bf7&oe=5F0FF302" alt="1" class="w-100 pt-4">
         </div>
     </div>
 </div>
